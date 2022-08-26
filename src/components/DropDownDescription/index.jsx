60389ff58893e1560c707cf22bb261bd639ab423
touatr <touati.ram@gmail.com>
2022-08-26 16:27:50 +0200

    details page ok

---
+++ src/components/DropDownDescription/index.jsx
@@ -0,0 +1,46 @@
+import React, { useState, useEffect} from 'react'
+import { useParams } from 'react-router-dom' 
+import '../../styles/DropDownDetails.css'
+import cover  from '../../assets/downIcon.png'
+
+function DropDownDescription({ logements }) {
+     // Get the userId param from the URL.
+     let { logementId } = useParams()
+     const [ logement, setLogement ] = useState(undefined)
+     useEffect(() => {
+         if(logement === undefined) {
+             const tmpLogement = logements.find(logement => logement.id == logementId)
+             if(tmpLogement === null) {
+                 //rediriger vers 404
+             }
+             setLogement(tmpLogement)
+         }
+     })
+    const [active, setActive] = useState(false)
+
+    return active ? (
+            <div className='drop-downs'>
+                <div className="drop-down-details">
+                    <div className='title-img' >
+                        <h2>Description</h2>
+                        <img  className='hidden-icon' src={cover} alt="down-icon"
+                        onClick={() => setActive(false)}/>
+                    </div>
+                    <div className='drop-down-text' ><p>{logement && logement.description}</p></div>
+                </div>
+                <br/>
+            </div>
+        ) : (
+        <div className='drop-downs'>
+            <div className="drop-down-details">
+                <div className='title-img' >
+                    <h2>Description</h2>
+                    <img className='hidden-icon display-icon' src={cover} alt="down-icon"
+                    onClick={() => setActive(true)}/>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default DropDownDescription
--- src/components/DropDownDetails/index.js
+++ src/components/DropDownDetails/index.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom' 
 import '../../styles/DropDownDetails.css'
 import cover  from '../../assets/downIcon.png'
+import DropDownDescription from '../DropDownDescription'
+import DropDownEquipment from '../DropDownEquipment'
 
 function DropDownDetails({ logements }) {
      // Get the userId param from the URL.
@@ -16,23 +18,33 @@ function DropDownDetails({ logements }) {
              setLogement(tmpLogement)
          }
      })
-    const [active, setActive] = useState(-1)
-    function handleClick(index) {
-        
-        setActive(index === active ? -1 : index)
-    }
-    return (
+    const [active, setActive] = useState(false)
+
+     return (
+        <div className='drop-downs' >
+             <DropDownDescription logements={logements}/>
+             <DropDownEquipment logements={logements}/>
+        </div>
+     )
+
+    return active ? (
         <div className='container'>
             {
                 <div className='drop-downs'>
                     <div className="drop-down-details">
-                        <h2>Description</h2>
-                        <img src={cover} alt="down-icon"/>
+                        <div className='title-img' >
+                            <h2>Description</h2>
+                            <img  className='hidden-icon' src={cover} alt="down-icon"
+                            onClick={() => setActive(false)}/>
+                        </div>
                         <div className='drop-down-text' ><p>{logement && logement.description}</p></div>
                     </div>
                     <div className="drop-down-details">
-                        <h2>Equipements</h2>
-                        <img src={cover} alt="down-icon"/>
+                        <div className='title-img' >
+                            <h2>Equipements</h2>
+                            <img  className='hidden-icon' src={cover} alt="down-icon"
+                            onClick={() => setActive(false)}/>
+                        </div>
                         {
                             <div className='drop-down-text'>
                                 {logement && logement.equipments.map((equipment) => (
@@ -41,6 +53,27 @@ function DropDownDetails({ logements }) {
                             </div>
                         }
                         <br/>
+                    </div>
+                </div>
+            }
+        </div>
+    ) : (
+        <div className='container'>
+            {
+                <div className='drop-downs'>
+                    <div className="drop-down-details">
+                        <div className='title-img' >
+                            <h2>Description</h2>
+                            <img className='hidden-icon display-icon' src={cover} alt="down-icon"
+                            onClick={() => setActive(true)}/>
+                        </div>
+                    </div>
+                    <div className="drop-down-details">
+                        <div className='title-img' >
+                            <h2>Equipements</h2>
+                            <img className='hidden-icon display-icon' src={cover} alt="down-icon"
+                            onClick={() => setActive(true)}/>
+                        </div>
                     </div>
                 </div>
             }
